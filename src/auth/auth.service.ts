cec432100ade749f200d914fa6f47cb4e9fd2f8b
Matheus Rafael <matheusrafael77@hotmail.com>
2025-08-16 22:35:22 -0300

    feat ajusta output do prisma

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { AuthDtoRequest } from './dto/auth-dto-request';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+    constructor(private prismaService: PrismaService) {}
     login(body: AuthDtoRequest): { token: string } {
         console.log(body);
-
         return { token: '1234' };
     }
 }
